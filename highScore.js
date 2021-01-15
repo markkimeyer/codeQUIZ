061fa5375d29f16d36363947030aa106e0b8cc74
Markki Meyer <markkimeyer@gmail.com>
2021-01-14 22:36:45 -0600

    submit button

--- highScore.js
+++ highScore.js
@@ -1,3 +1,7 @@
-var hallOfFame = document.querySelector(".highScore");
+var hallOfFame = document.querySelector("#initials");
 var initials = localStorage.getItem("HOF");
 hallOfFame.textContent = initials;
+
+var record = document.querySelector("#score");
+var highScore = localStorage.getItem("score");
+record.textContent = highScore;
--- scriptdoc.js
+++ scriptdoc.js
@@ -16,26 +16,27 @@ var timer = document.querySelector("#countdownSpan");
 var timerText = 40;
 var countdown;
 
-startButton.addEventListener("click", function() {
-   alert("You have " + timerText + " seconds to complete the quiz. Click OK to begin.");
-   myTimer();
-   questionOne();
+startButton.addEventListener("click", function () {
+    alert("You have " + timerText + " seconds to complete the quiz. Click OK to begin.");
+    myTimer();
+    questionOne();
 })
 
 
-function myTimer() {  
-   countdown = setInterval(function() {   
-    timerText--;
-    timer.textContent = "Time Left: " + timerText;
-    if (timerText <= 0 ) {
-        clearInterval(countdown);
-        timer.textContent = "TIME IS UP!"
-        gameOver(timerText);
+function myTimer() {
+    countdown = setInterval(function () {
+        timerText--;
+        timer.textContent = "Time Left: " + timerText;
+        //this is if you run out the clock 
+        if (timerText <= 0) {
+            clearInterval(countdown);
+            timer.textContent = "TIME IS UP!";
+            gameOver(timerText);
 
 
-    }
+        }
 
-}, 1000)
+    }, 1000)
 }
 
 
@@ -48,133 +49,152 @@ function myTimer() {
 //     }
 
 function questionOne() {
-startButton.style.display = "none"; 
-bubbles.style.display = "block";
-header.innerHTML = "<h3>What breed of dog is this?</h3>";
-image.src = "images/dalmation.jpg";
+    startButton.style.display = "none";
+    bubbles.style.display = "block";
+    header.innerHTML = "<h3>What breed of dog is this?</h3>";
+    image.src = "images/dalmation.jpg";
 
     answerOne.innerHTML = "<b>Dalmation</b>";
-    
+
     answerOne.addEventListener("click", function () {
         correct();
-     });
+    });
 
     answerTwo.innerHTML = "<b>Labrador Retriever</b>";
-    
+
     answerTwo.addEventListener("click", function () {
         incorrect();
-     });
+    });
 
     answerThree.innerHTML = "<b>Fire Truck</b>";
-    
+
     answerThree.addEventListener("click", function () {
         incorrect();
-     });
+    });
 
     answerFour.innerHTML = "<b>Polka Dot</b>";
-    
+
     answerFour.addEventListener("click", function () {
         incorrect();
-     });
+    });
 }
 
 function questionTwo() {
-    startButton.style.display = "none"; 
+    startButton.style.display = "none";
     bubbles.style.display = "block";
     header.innerHTML = "<h3>What breed of dog is this?</h3>";
     image.src = "images/gshort.jpg";
-    
-        answerOne.innerHTML = "<b>English Pointer</b>";
-        
-        answerOne.addEventListener("click", function () {
-            correct();
-         });
-    
-        answerTwo.innerHTML = "<b>German Shepherd</b>";
-        
-        answerTwo.addEventListener("click", function () {
-            incorrect();
-         });
-    
-        answerThree.innerHTML = "<b>German Shorthair Pointer</b>";
-        
-        answerThree.addEventListener("click", function () {
-            correct();
-         });
-    
-        answerFour.innerHTML = "<b>Polka Dot</b>";
-        
-        answerFour.addEventListener("click", function () {
-            incorrect();
-         });
-    }   
 
-    function questionThree() {
-        startButton.style.display = "none"; 
-        bubbles.style.display = "block";
-        header.innerHTML = "<h3>What breed of dog is this?</h3>";
-        image.src = "images/ausshep.jpg";
-        
-            answerOne.innerHTML = "<b>Ausiedoodle</b>";
-            
-            answerOne.addEventListener("click", function () {
-                incorrect();
-             });
-        
-            answerTwo.innerHTML = "<b>Australian Shepherd</b>";
-            
-            answerTwo.addEventListener("click", function () {
-                correct();
-                gameOver(timerText + 20);
-             });
-        
-            answerThree.innerHTML = "<b>English Setter</b>";
-            
-            answerThree.addEventListener("click", function () {
-                incorrect();
-             });
-        
-            answerFour.innerHTML = "<b>Polka Dot</b>";
-            
-            answerFour.addEventListener("click", function () {
-                incorrect();
-             });
-        }   
+    answerOne.innerHTML = "<b>English Pointer</b>";
+
+    answerOne.addEventListener("click", function () {
+        incorrect();
+    });
+
+    answerTwo.innerHTML = "<b>German Shepherd</b>";
+
+    answerTwo.addEventListener("click", function () {
+        incorrect();
+    });
+
+    answerThree.innerHTML = "<b>German Shorthair Pointer</b>";
+
+    answerThree.addEventListener("click", function () {
+        correct();
+    });
+
+    answerFour.innerHTML = "<b>Polka Dot</b>";
+
+    answerFour.addEventListener("click", function () {
+        incorrect();
+    });
+}
+
+function questionThree() {
+    startButton.style.display = "none";
+    bubbles.style.display = "block";
+    header.innerHTML = "<h3>What breed of dog is this?</h3>";
+    image.src = "images/ausshep.jpg";
+
+    answerOne.innerHTML = "<b>Ausiedoodle</b>";
+
+    answerOne.addEventListener("click", function () {
+        incorrect();
+    });
+
+    answerTwo.innerHTML = "<b>Australian Shepherd</b>";
+
+    answerTwo.addEventListener("click", function () {
+        gameOver(timerText);
+    });
+
+    answerThree.innerHTML = "<b>English Setter</b>";
+
+    answerThree.addEventListener("click", function () {
+        incorrect();
+    });
+
+    answerFour.innerHTML = "<b>Polka Dot</b>";
+
+    answerFour.addEventListener("click", function () {
+        incorrect();
+    });
+}
 
 //Game Over
 function gameOver(timerText) {
     timer.style.display = "none";
     clearInterval(countdown);
     var score = timerText;
-   alert("Quiz Over! Your score is " + score + "!");
-    var initials = prompt("Enter your initials to record them to the Hall of Fame!");
-    localStorage.setItem("HOF", initials);
-    localStorage.setItem("score", score);
-    console.log(initials);
-    console.log(score);
+    // console.log(score);
+    // alert("Quiz Over! Your score is " + score + "!");
+    var inputEl = document.createElement("input");
+    inputEl.setAttribute("id", "initialInput");
+    inputEl.setAttribute("type", "text");
+    console.log(inputEl);
+
+    var buttonEl = document.createElement("button");
+    buttonEl.setAttribute("id", "submitScore");
+    buttonEl.innerHTML = "Submit your initials";
+    console.log("This is the button element for submit button",buttonEl);
+
+
+
+    var initials = document.getElementById("enterScore");
+    initials.appendChild(inputEl);
+    initials.appendChild(buttonEl);
+
+
+    document.getElementById("submitScore").addEventListener("click", function () {
+    var initialsText = document.getElementById("initialInput").value;
+        localStorage.setItem("HOF", initialsText);
+        localStorage.setItem("score", score);
+        console.log(initialsText);
+        console.log(score);
+    }
+    )
 
 }
 
 
 function correct() {
-    questionNumber ++;
+    questionNumber++;
     nextQuestion();
 }
 
-function nextQuestion () {
-if (questionNumber === 2) {
-    questionTwo();
-};
-if (questionNumber === 3) {
-    questionThree();
-}
-}
-    function incorrect () {
-       timer.textContent = "Time Left: " + timerText;
-       timerText -= 10;
-       if (timerText <= 0 ) {
-        clearInterval(countdown);
-        timer.textContent = "TIME IS UP!"
-        gameOver();
-       }
+function nextQuestion() {
+    if (questionNumber === 2) {
+        questionTwo();
+    };
+    if (questionNumber === 3) {
+        questionThree();
+
     }
+}
+function incorrect() {
+    timer.textContent = "Time Left: " + timerText;
+    timerText -= 10;
+    questionNumber++;
+    nextQuestion();
+
+}
